aecc01807fd41f73a35ebcb9c547e4e915833305
alessandra.nsantos <alessandra.nsantos@b2wdigital.com>
2021-07-18 10:59:08 -0300

    ignite: props of the father for child

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,27 +1,21 @@
+import { RepositoryItem } from './RepositoryItem';
+
+const repository = {
+    name: 'teste2',
+    description: 'Forms in React',
+    link: 'https://github.com/unform/unform'
+}
+
 export function RepositoryList() {
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <li>
-                    <strong>Teste</strong>
-                    <p>Teste</p>
-
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <strong>Teste</strong>
-                    <p>Teste</p>
-
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
             </ul>
         </section>
     )
